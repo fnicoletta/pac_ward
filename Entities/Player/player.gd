d6e61820f5ae62a7ba358011251c367e790602e4
Franky Khoury <fnicoletta97@gmail.com>
2025-01-12 02:18:09 -0800

    audio

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -10,6 +10,7 @@ extends CharacterBody3D
 var score: int = 0
 
 @onready var camera: Camera3D = %Camera3D
+@onready var footsteps: AudioStreamPlayer3D = %Footsteps
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -33,9 +34,12 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		if not footsteps.playing:
+			footsteps.play()
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
 	else:
+		footsteps.stop()
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
 
--- Items/Basic/basic.gd
+++ Items/Basic/basic.gd
@@ -1,7 +1,14 @@
 extends Area3D
 
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var sphere: CSGSphere3D = $CGSphere3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
-		body.update_score()
-		queue_free()
+		sphere.visible = false
+		audio_stream_player.play()
+		audio_stream_player.connect("finished", Callable(self, "_on_audio_finished").bind(body))
+
+func _on_audio_finished(body: Player) -> void:
+	body.update_score()
+	queue_free()
